f925e2950e4c79bf0d9675d5fe1fa658a699f3e8
fj <fj@somedomain.com>
2024-12-22 19:54:57 -0800

    practicing some makefile concepts

---
+++ sokol_check/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <linked_list.h>
+
+int main(int argc, char** argv){
+    printf("hello\n");
+    printf("bye\n");
+    printf("later\n");
+}
---
+++ sokol_check/linked_list.h
@@ -0,0 +1,9 @@
+#ifndef LINKED_LIST_HEADER
+#define LINKED_LIST_HEADER
+
+struct {
+    int foo;
+    float bar;
+} linked_list_node;
+
+#endif
