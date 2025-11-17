4a25487f8d878cfb0847c373f7a70c77d44490e1
fj <fj@somedomain.com>
2025-11-16 22:21:08 -0800

    suppressing text working pretty well, using termios.

--- console_arcade/README.txt
+++ console_arcade/README.txt
@@ -1,3 +1,7 @@
 # https://codeberg.org/remoof/crokey
 
 gcc snake.c -lX11
+
+
+
+https://www.reddit.com/r/C_Programming/comments/v5k3z1/reading_char_for_char_from_stdin_without_waiting/
--- console_arcade/snake.c
+++ console_arcade/snake.c
@@ -5,7 +5,38 @@
 #include <termios.h>
 #include <unistd.h>
 
+#define ERASE_LINE "\033[2K\r"
+
 int main(int argc, char** argv){
+
+	//struct termios orig_termios;
+
+	//void die(const char *s) {
+	//    perror(s);
+	//    exit(1);
+	//}
+
+	//void disable_raw_mode() {
+	//    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+	//	die("tcsetattr");
+	//    }
+	//}
+
+	//void enable_raw_mode() {
+	//    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+	//	die("tcgetattr");
+	//    }
+	//    atexit(disable_raw_mode);  // Restore on exit
+
+	//    struct termios raw = orig_termios;
+	//    raw.c_lflag &= ~(ICANON | ECHO);  // Disable canonical mode and echo
+	//    raw.c_cc[VMIN] = 1;  // Read at least 1 byte
+	//    raw.c_cc[VTIME] = 0; // No timeout
+
+	//    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+	//	die("tcsetattr");
+	//    }
+	//}
 
 	//freopen("/dev/null", "r", stdin);
 	
@@ -13,8 +44,13 @@ int main(int argc, char** argv){
 	struct termios term_curr, term_prev;
 	tcgetattr(STDIN_FILENO, &term_prev);
 	term_curr = term_prev;
-	term_curr.c_lflag &= ~ECHO;
+	//term_curr.c_lflag &= ~ECHO;
+	term_curr.c_lflag &= (~ICANON & ~ECHO);
+	term_curr.c_cc[VMIN] = 1;
+	term_curr.c_cc[VTIME] = 0;
 	tcsetattr(STDIN_FILENO, TCSANOW, &term_curr);
+	
+	//enable_raw_mode();
 
 	//printf("hello\n");
 	time_t time_curr;
@@ -28,18 +64,38 @@ int main(int argc, char** argv){
 			char_last = char_curr;
 			if(char_curr != KEY_LIST_NONE){
 				//printf("%c\n", c);
+				//printf("\r\033[K"); // clear line
+				printf(ERASE_LINE);
 				printf("%s %ld\n", crokey_enum_to_string(char_curr), (long)time_curr);
 			}
 		}
 		
 
 		if(char_curr == KEY_Q){
+		//if(char_curr == KEY_ESCAPE){
 			break;
 		}
 
 	}
 
 	// restore terminal echo
+	sleep(1);
 	tcsetattr(STDIN_FILENO, TCSANOW, &term_prev);
+
+	// ??
+	//scanf("%*[^\n]");
+	
+	//fseek(stdin, 0, SEEK_END);
+	
+	//char c;
+	//while(c = getchar());
+	
+	//for(int i = 0; i < 1000; i++){
+	//	write(STDOUT_FILENO, "\b \b", 3);
+	//}
+	
+	//fflush(stdin);
+	tcflush(STDIN_FILENO, TCIFLUSH);
+	
 	return 0;
 }
