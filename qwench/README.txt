688f98dc60c9202ef7fd6d45764e38737cbf98bb
fj <fj@somedomain.com>
2026-03-16 22:08:40 -0700

    one simple example of drawing colored text in a loop is working. not sure if optimal.

--- qwench/README.txt
+++ qwench/README.txt
@@ -6,3 +6,7 @@ https://github.com/sahaj-b/go-attend
 
 considering crossterm for terminal management.
 
+
+TODO:
+- add command line arg for passing dictionary file.
+- add command line args to change game grid size, word speed, etc.
--- qwench/src/main.rs
+++ qwench/src/main.rs
@@ -3,8 +3,10 @@ use std::time::Duration;
 use std::time::Instant;
 
 use crossterm::{execute, queue};
-use crossterm::style::{Print, Stylize, Color, SetForegroundColor, ResetColor};
-use std::io::{stdout, Write};
+use crossterm::cursor::MoveTo;
+use crossterm::style::{Print, Color, SetForegroundColor, ResetColor};
+use crossterm::terminal::{Clear, ClearType};
+use std::io::{Write};
 
 use crossterm::{
     event::{self, Event, KeyCode},
@@ -14,30 +16,15 @@ use crossterm::{
 fn main() -> io::Result<()> {
     enable_raw_mode()?;
 
-    //let mut stdout = io::stdout();
-    //let t_beg = Instant::now();
-    //let t_tick_ms = 2000;
-    //let mut t_rem = 
-    //loop {
-    //    if event::poll(Duration::from_millis(1200))? {
-    //        // Read the event if available
-    //        if let Event::Key(event) = event::read()? {
-    //            //println!("Key pressed: {:?}\r", event.code);
-    //            print!("Key pressed: {:?}\r\n", event.code);
-    //            //stdout.write_all(b"Key pressed:\r\n")?;
-    //            //execute!(stdout, Print("foo\r\n"))?;
-    //            if event.code == KeyCode::Char('q') {
-    //                //println!("Quitting...");
-    //                break;
-    //            }
-    //        }
-    //    } else {
-    //        println!("No event available.\r\n");
-    //    }
-    //}
+    // initialize the render grid.
+    //const GRID_W: usize = 24;
+    //const GRID_H: usize = 24;
+    //let mut grid = [['.'; GRID_W]; GRID_H];
+
+    let mut buffer: Vec<(char, Option<Color>)> = vec![];
 
     let mut stdout = io::stdout();
-    let tick_ms = Duration::from_millis(1000);
+    let tick_ms = Duration::from_millis(200);
     let time_beg = Instant::now();
     let mut quit = false;
     let mut remaining_ms = tick_ms;
@@ -56,6 +43,40 @@ fn main() -> io::Result<()> {
         }
         else{
             print!("drawing...\r\n");
+
+            execute!(stdout, Clear(ClearType::All), MoveTo(0,0))?;
+            
+            buffer.push(('H', Some(Color::Red)));
+            buffer.push(('e', None));
+            buffer.push(('l', None));
+            buffer.push(('l', None));
+            buffer.push(('o', Some(Color::Green)));
+            buffer.push((' ', None));
+            buffer.push(('W', Some(Color::Blue)));
+            buffer.push(('o', None));
+            buffer.push(('r', Some(Color::Yellow)));
+            buffer.push(('l', None));
+            buffer.push(('d', Some(Color::Magenta)));
+            buffer.push(('!', None));
+            buffer.push(('\r', None));
+            buffer.push(('\n', None));
+
+            
+            for (ch, maybe_color) in &buffer {
+                if let Some(color) = maybe_color {
+                    queue!(
+                        stdout,
+                        SetForegroundColor(*color),
+                        Print(ch),
+                        ResetColor
+                    )?;
+                } else {
+                    queue!(stdout, Print(ch))?;
+                }
+            }
+
+            stdout.flush()?;
+
             let rem = tick_ms.as_millis() - (time_beg.elapsed().as_millis() % tick_ms.as_millis());
             remaining_ms = Duration::from_millis(rem as u64);
         }
